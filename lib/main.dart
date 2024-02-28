92a562c15d69322168adae94b5420a825491bbbb
LUIS_BRANDON_CRISTOBAL_RAMIREZ <a.21308051280456@cbtis128.edu.mx>
2024-02-28 15:15:46 +0000

    Actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiListaCard());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiListaCard extends StatelessWidget {
+  const MiListaCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Mi ListView Cristobal',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: const InicioDePaguina());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class InicioDePaguina extends StatefulWidget {
+  const InicioDePaguina({Key? key}) : super(key: key);
 
   @override
+  State<InicioDePaguina> createState() => _InicioDePaguinaState();
+}
+
+class _InicioDePaguinaState extends State<InicioDePaguina> {
+  @override
+  List<String> images = [
+    "assets/images/a1.png",
+    "assets/images/a2.png",
+    "assets/images/a3.jpg",
+    "assets/images/a4.png",
+    "assets/images/a5.png",
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          title: const Text('ListView Cristobal'),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemBuilder: (BuildContext, index) {
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(images[index]),
+                ),
+                title: Text("El Titulo"),
+                subtitle: Text("El Subtitulo"),
+              ),
+            );
+          },
+          itemCount: images.length,
+          shrinkWrap: true,
+          padding: EdgeInsets.all(5),
+          scrollDirection: Axis.vertical,
+        ));
   }
 }
